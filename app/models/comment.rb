06753f10ed98c1c7a39df752f609e56117651135
Menanya <emmiemenz@gmail.com>
2022-09-15 19:22:10 +0100

    fix comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
 
-    def comment_counter_update
-        post.comments_counter 0 unless Post.comments_counter?
-        post.comments_counter += 1
-        post.save
-        end
-    end
+  after_create :comment_counter_update
+
+  def comment_counter_update
+    post.comments_counter 0 unless Post.comments_counter?
+    post.comments_counter += 1
+    post.save
+  end
 end
