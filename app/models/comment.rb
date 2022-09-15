42378e916a97ff1286cdc31c9eab5fe124a52d05
Menanya <emmiemenz@gmail.com>
2022-09-15 19:48:39 +0100

    create after save comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
-  after_create :comment_counter_update
+  after_save :comment_counter_update
 
   def comment_counter_update
     post.comments_counter 0 unless Post.comments_counter?
